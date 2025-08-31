2659012fe72f36848b77eac8e3e23ccdad8eff2e
sourab <sourab@sourab.sourab.com>
2025-08-31 12:57:51 +0530

    Router: add router in main and route to dashboard

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -24,3 +24,23 @@
 //         </div>
 //     </Provider>
 // )
+//working on the router
+// import React from "react";
+// import { Provider } from 'react-redux'
+// import { store } from "../store";
+// import { ConnectedDashboard } from "./dashboard";
+// import { Router, Route} from 'react-router-dom'
+// import { history } from "../store/history";
+
+
+// export const Main = () => (
+//     <Router history={history}>
+//         <Provider store={store}>
+//         <div>
+//             {/* Dashboard Goes here */}
+//             <Route exact path="/dashboard" render={()=><ConnectedDashboard/>}></Route>
+//             {/* the exact attribute says to display the comp only if the url matches the exact path */}
+//         </div>
+//         </Provider>
+//     </Router>
+// );
